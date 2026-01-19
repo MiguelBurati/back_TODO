157963eaa4958344510f6428e88cba46a6fc279d
Miguel-Burati <269341@unifio.edu.br>
2026-01-19 14:08:47 -0300

    crud do user

---
+++ proj/app.py
@@ -0,0 +1,117 @@
+import mysql.connector
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import os
+
+app = Flask(__name__)
+CORS(app)
+
+#realiza a conexao com o db
+def get_db():
+    try:
+        db = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="1805",
+            database="tarefas_db"
+        )
+        print("Banco conectado com sucesso")
+        return db
+    except mysql.connector.Error as e:
+        print("Erro ao conectar:", e)
+        return None
+    
+
+#criar usuario (POST)
+@app.route('/users', methods=['POST'])
+def add_user():
+    nome = request.json.get('nome')
+    senha = request.json.get('senha')
+
+    if not nome or not senha:
+        return jsonify({'error' : 'Os dados sao obrigatorios'}), 400
+    
+    try:
+        db = get_db()
+        cursor = db.cursor()
+        cursor.execute('INSERT INTO users (nome, senha) VALUES (%s, %s)', (nome, senha))
+        db.commit()
+        return jsonify({'message': 'dado gravado com sucesso'}), 201
+    except mysql.connector.Error as e:
+        return jsonify({'error': str(e)}), 500    
+    finally:
+        db.close()
+
+#consultar usuarios existentes (GET)
+@app.route('/users', methods=['GET'])
+def get_users():
+    try:
+        db = get_db()
+        cursor = db.cursor()
+        cursor.execute('SELECT * FROM users')
+        #logica para consultar e transformar os dados // o cursor.description coleta os nomes das colunas
+        columns = [col[0] for col in cursor.description]
+        users = [dict(zip(columns, row)) for row in cursor.fetchall()]
+        return jsonify(users)
+    except mysql.connector.Error as e:
+        return jsonify({'error': str(e)}), 500    
+    finally:
+        db.close()
+
+#consultar usuario pelo id (GET)
+@app.route('/users/<int:user_id>', methods=['GET'])
+def get_user(user_id):
+    try:
+        db = get_db()
+        cursor = db.cursor()
+        cursor.execute('SELECT * FROM users WHERE id = %s', (user_id,)) #ao final tem que ter "," para reconhecer como tupla
+        columns = [col[0] for col in cursor.description]
+        row = cursor.fetchone() #ao invers de fetchall pois retornara so um dicionario
+        if row:
+            user = dict(zip(columns, row)) #vai transformar a linha em dicionario -> chave = nome da coluna || valor = dado.
+            return jsonify(user)
+        else: 
+            return jsonify({'error': 'Dado nao encontrado'}), 404
+    except mysql.connector.Error as e:
+        return jsonify({'error': str(e)}), 500    
+    finally:
+        db.close()
+
+#atualizar dado (PUT)
+@app.route('/users/<int:user_id>', methods=['PUT'])
+def put_user(user_id):
+
+    nome = request.json.get('nome')
+    senha = request.json.get('senha')
+
+    if not nome or not senha:
+        return jsonify({'error' : 'Os dados sao obrigatorios'}), 400
+    
+    try:
+        db = get_db()
+        cursor = db.cursor()
+        cursor.execute('UPDATE users SET nome = %s, senha = %s WHERE id = %s', (nome, senha, user_id))
+        db.commit()
+        return jsonify({'message': 'Usuario atualizado com sucesso'}), 201
+    except mysql.connector.Error as e:
+        return jsonify({'error': str(e)}), 500    
+    finally:
+        db.close()
+
+#deletar dado (DELETE)
+@app.route('/users/<int:user_id>', methods=['DELETE'])
+def del_user(user_id):
+    try:
+        db = get_db()
+        cursor = db.cursor()
+        cursor.execute('DELETE FROM users WHERE id = %s', (user_id,))
+        db.commit()
+        return jsonify({'message' : 'Dado deletado com sucesso'}), 200
+    except mysql.connector.Error as e:
+        return jsonify({'error': str(e)}), 500    
+    finally:
+        db.close()
+
+if __name__ == '__main__':
+    get_db()
+    app.run(debug=True)
